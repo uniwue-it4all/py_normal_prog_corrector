4a96fac499145d7ca93e1ab4925e004257aac377
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2020-06-09 15:25:05 +0200

    update test script

--- run_docker.sh
+++ run_docker.sh
@@ -3,43 +3,50 @@
 EX=${1:?"Error: exercise name / folder is not defined!"}
 
 if [[ ${EX} == */ ]]; then
-    EX=${EX::-1}
+  EX=${EX::-1}
 fi
 
 ERROR_SUFFIX=${2:-""}
 
 SOL_FILE=${EX}/${EX}.py
 
-IMG_NAME=beyselein/py_normal_prog_corrector
+IMG_VERSION=${IMG_VERSION:-latest}
+IMG_NAME=py_normal_prog_corrector
+
+IMG_TAG=${IMG_NAME}:${IMG_VERSION}
 
 # Build image
-#docker build -t ${IMG_NAME} .
+docker build -t ${IMG_TAG} .
 
 SOL_FILE_NAME=${EX}${ERROR_SUFFIX}
+
 SOL_FILE=${EX}/${SOL_FILE_NAME}.py
 if [[ ! -f ${SOL_FILE} ]]; then
-    echo "The solution file ${SOL_FILE} does not exist!"
-    exit 1
+  echo "The solution file ${SOL_FILE} does not exist!"
+  exit 1
 fi
 
 TEST_FILE=${EX}/${EX}_test.py
 if [[ ! -f ${TEST_FILE} ]]; then
-    echo "The test file ${TEST_FILE} does not exist!"
-    exit 2
+  echo "The test file ${TEST_FILE} does not exist!"
+  exit 2
 fi
 
 RES_FILE=results/${SOL_FILE_NAME}_result
+
 if [[ ! -f ${RES_FILE} ]]; then
-    if [[ ! -d results ]]; then
-        mkdir results
-    fi
-    touch ${RES_FILE}
+  if [[ ! -d results ]]; then
+    mkdir results
+  fi
+  touch "${RES_FILE}"
 else
-    > ${RES_FILE}
+  # shellcheck disable=SC2188
+  >"${RES_FILE}"
 fi
 
-
 docker run -it --rm \
-    -v $(pwd)/${SOL_FILE}:/data/${EX}.py \
-    -v $(pwd)/${TEST_FILE}:/data/${EX}_test.py \
-    ${IMG_NAME} > ${RES_FILE}
+  -v "$(pwd)/${SOL_FILE}:/data/${EX}.py" \
+  -v "$(pwd)/${TEST_FILE}:/data/${EX}_test.py" \
+  ${IMG_NAME} >"${RES_FILE}"
+
+cat "${RES_FILE}"
